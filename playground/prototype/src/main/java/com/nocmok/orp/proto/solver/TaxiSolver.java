b253ce632e663338dfd74dadded0e63b22d7624d
Николай Сафонов <uporoty-cot@mail.ru>
2022-01-19 22:46:04 +0300

    improve taxi solver

--- playground/prototype/src/main/java/com/nocmok/orp/proto/solver/TaxiSolver.java
+++ playground/prototype/src/main/java/com/nocmok/orp/proto/solver/TaxiSolver.java
@@ -4,9 +4,10 @@ import com.nocmok.orp.proto.graph.Graph;
 import com.nocmok.orp.proto.pojo.GPS;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -35,9 +36,66 @@ public class TaxiSolver implements ORPSolver {
                 .orElse(0);
     }
 
+    // Объединяет связанные маршруты.
+    // Последний элемент одного маршрута должен совпадать с первым элементом следующего
+    private Route combineRoutes(List<Route> routes) {
+        List<Integer> combinedRoute = new ArrayList<>();
+        double combinedRouteDistance = 0;
+        for (var route : routes) {
+            if (!combinedRoute.isEmpty()) {
+                if (!Objects.equals(combinedRoute.get(combinedRoute.size() - 1), route.getRoute().get(0))) {
+                    throw new RuntimeException("routes expected to follow each other, " +
+                            "but last node of preceding route was not equal to first node of the following route");
+                }
+                combinedRoute.remove(combinedRoute.size() - 1);
+            }
+            combinedRoute.addAll(route.getRoute());
+            combinedRouteDistance += route.getDistance();
+        }
+        return new Route(combinedRoute, combinedRouteDistance);
+    }
+
     // Правая часть не включается
     private boolean checkTimeFrame(int time, int earlyBound, int lateBound) {
         return time >= earlyBound && time < lateBound;
+    }
+
+    private Optional<Matching> matchPendingVehicle(Request request, Vehicle vehicle) {
+        int vehicleNode = closestNode(state.getGraph(), vehicle.getGpsLog().get(vehicle.getGpsLog().size() - 1));
+        Route routeToClient =
+                shortestPathSolver.dijkstra(state.getGraph(), vehicleNode, request.getDepartureNode());
+        int timeToClient = (int) (routeToClient.getDistance() / vehicle.getAvgVelocity());
+
+        if (!checkTimeFrame(state.getTime() + timeToClient, request.getEarliestDepartureTime(), request.getLatestDepartureTime())) {
+            return Optional.empty();
+        }
+
+        var routeWithClient = shortestPathSolver.dijkstra(state.getGraph(), request.getDepartureNode(), request.getArrivalNode());
+        var fullRoute = combineRoutes(List.of(routeToClient, routeWithClient));
+        var schedule = List.of(new ScheduleCheckpoint(request, request.getDepartureNode()),
+                new ScheduleCheckpoint(request, request.getArrivalNode()));
+
+        return Optional.of(new Matching(vehicle, fullRoute, schedule));
+    }
+
+    private Optional<Matching> matchServingVehicle(Request request, Vehicle vehicle) {
+        // найти приблизительное время в конечной точке текущего плана
+        int vehicleNextNode = vehicle.getNextNode().get();
+        int vehicleLastNode = vehicle.getRoute().get(vehicle.getRoute().size() - 1);
+        var currentRoute = shortestPathSolver.dijkstra(state.getGraph(), vehicleNextNode, vehicleLastNode);
+        var routeToNewClient = shortestPathSolver.dijkstra(state.getGraph(), vehicleLastNode, request.getDepartureNode());
+
+        if ((currentRoute.getDistance() + routeToNewClient.getDistance()) / vehicle.getAvgVelocity() > request.getLatestDepartureTime()) {
+            return Optional.empty();
+        }
+
+        var newClientRoute = shortestPathSolver.dijkstra(state.getGraph(), request.getDepartureNode(), request.getArrivalNode());
+        var fullRoute = combineRoutes(List.of(currentRoute, routeToNewClient, newClientRoute));
+        var schedule = new ArrayList<>(vehicle.getCurrentSchedule());
+        schedule.add(new ScheduleCheckpoint(request, request.getDepartureNode()));
+        schedule.add(new ScheduleCheckpoint(request, request.getArrivalNode()));
+
+        return Optional.of(new Matching(vehicle, fullRoute, schedule));
     }
 
     @Override public Matching computeMatching(Request request) {
@@ -46,56 +104,50 @@ public class TaxiSolver implements ORPSolver {
         // Считает оптимальный маршрут для выполнения запроса
 
         // находим доступные мащины
-        var pendingVehicles = state.getVehicleList().stream()
-                .filter(vehicle -> vehicle.getState() == Vehicle.State.PENDING)
+        var candidateVehicles = state.getVehicleList().stream()
+                .filter(vehicle -> vehicle.getState() != Vehicle.State.AFK)
                 .collect(Collectors.toList());
 
-        if (pendingVehicles.isEmpty()) {
+        if (candidateVehicles.isEmpty()) {
             return new Matching(Matching.DenialReason.NO_VEHICLE_NEARBY);
         }
 
         // считаем для каждой машины ближайшую к ней ноду
-        Vehicle bestVehicle = null;
-        Route bestRouteToClient = new Route(Collections.emptyList(), Double.POSITIVE_INFINITY);
+        Matching bestMatching = null;
+        double bestRouteDistanceToClient = Double.POSITIVE_INFINITY;
 
-        for (var vehicle : pendingVehicles) {
-            int vehicleNode = closestNode(state.getGraph(), vehicle.getGpsLog().get(vehicle.getGpsLog().size() - 1));
-            Route routeToClient =
-                    shortestPathSolver.dijkstra(state.getGraph(), vehicleNode, request.getDepartureNode());
-            int timeToClient = (int) (routeToClient.getDistance() / vehicle.getAvgVelocity());
-
-            if (routeToClient.getDistance() < bestRouteToClient.getDistance()) {
-                bestVehicle = vehicle;
-                bestRouteToClient = routeToClient;
-            }
-
-            // проверяем временные рамки клиента
-            if (!checkTimeFrame(state.getTime() + timeToClient, request.getDepartureTimeWindow()[0],
-                    request.getDepartureTimeWindow()[1])) {
-                continue;
+        for (var vehicle : candidateVehicles) {
+            if (vehicle.getState() == Vehicle.State.PENDING) {
+                var matching = matchPendingVehicle(request, vehicle);
+                if (matching.isEmpty()) {
+                    continue;
+                }
+                int vehicleNode = closestNode(state.getGraph(), vehicle.getGpsLog().get(vehicle.getGpsLog().size() - 1));
+                var routeToClient = shortestPathSolver.dijkstra(state.getGraph(), vehicleNode, request.getDepartureNode());
+                if (routeToClient.getDistance() >= bestRouteDistanceToClient) {
+                    continue;
+                }
+                bestMatching = matching.get();
+                bestRouteDistanceToClient = routeToClient.getDistance();
+            } else if (vehicle.getState() == Vehicle.State.SERVING) {
+                var matching = matchServingVehicle(request, vehicle);
+                if (matching.isEmpty()) {
+                    continue;
+                }
+                int vehicleLastNode = vehicle.getRoute().get(vehicle.getRoute().size() - 1);
+                var routeToClient = shortestPathSolver.dijkstra(state.getGraph(), vehicleLastNode, request.getDepartureNode());
+                if (routeToClient.getDistance() >= bestRouteDistanceToClient) {
+                    continue;
+                }
+                bestMatching = matching.get();
+                bestRouteDistanceToClient = routeToClient.getDistance();
             }
         }
 
-        if (bestVehicle == null) {
+        if (bestMatching == null) {
             return new Matching(Matching.DenialReason.NO_VEHICLE_NEARBY);
         }
 
-        if (bestRouteToClient.getDistance() == Double.POSITIVE_INFINITY) {
-            return new Matching(Matching.DenialReason.OUT_OF_SERVICE_REGION);
-        }
-
-        // Лучший маршрут от точки посадки до точки прибытия клиента
-        Route routeWithClient =
-                shortestPathSolver.dijkstra(state.getGraph(), request.getDepartureNode(), request.getArrivalNode());
-        var completeRoute = new ArrayList<Integer>();
-        completeRoute.addAll(bestRouteToClient.getRoute().subList(0, bestRouteToClient.getRoute().size() - 1));
-        completeRoute.addAll(routeWithClient.getRoute());
-        double compleRouteDistance = bestRouteToClient.getDistance() + routeWithClient.getDistance();
-
-        List<ScheduleCheckpoint> schedule = new ArrayList<>();
-        schedule.add(new ScheduleCheckpoint(request, request.getDepartureNode()));
-        schedule.add(new ScheduleCheckpoint(request, request.getArrivalNode()));
-
-        return new Matching(bestVehicle, new Route(completeRoute, compleRouteDistance), schedule);
+        return bestMatching;
     }
 }
