538e495dbcd0edbd21907e1e5221b9cb9711ba08
Николай Сафонов <uporoty-cot@mail.ru>
2022-02-04 20:22:31 +0300

    add interfaces for road index

--- core-api/src/main/java/com/nocmok/orp/core_api/RoadIndex.java
+++ core-api/src/main/java/com/nocmok/orp/core_api/RoadIndex.java
@@ -1,2 +1,13 @@
-package com.nocmok.orp.core_api;public interface RoadIndex {
+package com.nocmok.orp.core_api;
+
+import java.util.List;
+
+// 1) Добавление точечных объектов в индекс
+// 2) Удаление объектов из индекса
+// 3) Получение метаданных компонентов графа
+// 4) Поиск кратчайших путей между нодами графа
+// 5) Поиск соседей в заданном радиусе (по кратчайшим маршрутам)
+public interface RoadIndex {
+    RoadRoute shortestRoute(RoadNode a, RoadNode b);
+    List<Vehicle> getNeighbors(GCS center, double radius);
 }
--- core-api/src/main/java/com/nocmok/orp/core_api/RoadNode.java
+++ core-api/src/main/java/com/nocmok/orp/core_api/RoadNode.java
@@ -1,2 +1,21 @@
-package com.nocmok.orp.core_api;public class RoadNode {
+package com.nocmok.orp.core_api;
+
+public class RoadNode {
+
+    private int nodeId;
+
+    private GCS coordinates;
+
+    public RoadNode(int nodeId, GCS coordinates) {
+        this.nodeId = nodeId;
+        this.coordinates = coordinates;
+    }
+
+    public int getNodeId() {
+        return nodeId;
+    }
+
+    public GCS getCoordinates() {
+        return coordinates;
+    }
 }
