4406d393a9bda7baf36484cbb7154828e61a1ffe
Николай Сафонов <uporoty-cot@mail.ru>
2022-04-19 01:31:27 +0300

    add simple routings

--- orp-dashboard/src/App.js
+++ orp-dashboard/src/App.js
@@ -3,16 +3,29 @@ import {MapScene} from "./components/map/MapScene";
 import {CreateSessionPanel} from "./components/operation/create_session_panel/CreateSessionPanel";
 import './App.css'
 import './style/Common.css'
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import {CreateRequestPanel} from "./components/operation/create_request_panel/CreateRequestPanel";
+import {OperationPanel} from "./components/operation/operation_panel/OperationPanel";
+import {Route, Routes, Navigate} from "react-router-dom";
 
 export class App extends Component {
 
     render() {
         return (
             <div className="App">
-                {/*<CreateSessionPanel di={this.props.di}></CreateSessionPanel>*/}
-                <CreateRequestPanel di={this.props.di}></CreateRequestPanel>
+                <Routes>
+                    <Route path={"/dashboard"} element={<OperationPanel di={this.props.di}></OperationPanel>}/>
+
+                    <Route path={"/sessions/create"}
+                           element={<CreateSessionPanel di={this.props.di}></CreateSessionPanel>}/>
+
+                    <Route path={"/requests/create"}
+                           element={<CreateRequestPanel di={this.props.di}></CreateRequestPanel>}/>
+
+                    <Route path="*" element={<Navigate to="/dashboard"/>}/>
+
+                </Routes>
+
                 <MapScene di={this.props.di}></MapScene>
             </div>
         )
--- orp-dashboard/src/components/operation/OperationPanel.js
+++
@@ -1,5 +0,0 @@
-import {Component} from "react";
-
-export class OperationPanel extends Component {
-
-}
