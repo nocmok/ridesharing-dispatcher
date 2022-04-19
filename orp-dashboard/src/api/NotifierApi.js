584a6c8111f7ab27cfa4117e888c3877174165e1
Николай Сафонов <uporoty-cot@mail.ru>
2022-04-19 18:07:15 +0300

    display routes

--- orp-dashboard/src/api/NotifierApi.js
+++ orp-dashboard/src/api/NotifierApi.js
@@ -5,6 +5,9 @@ export const NOTIFIER_URL = "http://localhost:8081"
 export const CONNECT_PATH = "/api/notifier/connect"
 export const TELEMETRY_TOPIC_PATH = "/topic/telemetry/gps"
 
+export const DRIVER_TOPIC_PATH = "/topic/driver"
+export const DRIVER_API_ASSIGN_REQUEST_PATH = "/assign_request"
+
 export function getStompClient() {
     return Stomp.over(new SockJS(NOTIFIER_URL + CONNECT_PATH))
 }
--- orp-dashboard/src/session/SessionListener.js
+++ orp-dashboard/src/session/SessionListener.js
@@ -12,11 +12,13 @@ export class SessionListener {
 
         this.telemetryEventHandlers = []
         this.disconnectEventHandler = []
+        this.assignRequestEventHandlers = []
     }
 
     connect() {
         this.stompClient.connect({}, () => {
             this.stompClient.subscribe(NotifierApi.TELEMETRY_TOPIC_PATH + "/" + this.sessionId, this.#handleTelemetry.bind(this))
+            this.stompClient.subscribe(NotifierApi.DRIVER_TOPIC_PATH + NotifierApi.DRIVER_API_ASSIGN_REQUEST_PATH + "/" + this.sessionId, this.#handleAssignRequestNotification.bind(this))
         })
     }
 
@@ -29,8 +31,16 @@ export class SessionListener {
         this.telemetryEventHandlers.forEach(handler => handler(telemetry))
     }
 
+    #handleAssignRequestNotification(assignRequestNotification) {
+        this.assignRequestEventHandlers.forEach(handler => handler(assignRequestNotification))
+    }
+
     addTelemetryEventHandler(handler) {
         this.telemetryEventHandlers.push(handler)
+    }
+
+    addAssignRequestNotificationHandler(handler) {
+        this.assignRequestEventHandlers.push(handler)
     }
 
     addDisconnectEventHandler(handler) {
