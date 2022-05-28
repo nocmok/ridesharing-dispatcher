5ec98e05ce3a3feee9cc321ca1878f499c7911fc
Николай Сафонов <uporoty-cot@mail.ru>
2022-05-28 18:28:16 +0300

    operation panel redesign

--- orp-dashboard/src/components/operation/operation_panel/OperationPanel.js
+++ orp-dashboard/src/components/operation/operation_panel/OperationPanel.js
@@ -1,17 +1,24 @@
 import {Component} from "react";
 import classes from "./OperationPanel.module.css"
 import {Link} from "react-router-dom";
+import {DemoWidget} from "./DemoWidget";
+import {AdminWidget} from "./AdminWidget";
 
 export class OperationPanel extends Component {
 
     render() {
         return (
             <div className={classes.OperationPanel}>
-                <div className="Heading1" style={{color: "#7C7C7C"}}>
+                <div className={"Heading1 " + classes.Title} style={{color: "#7c7c7c"}}>
                     Панель управления
                 </div>
-                <Link to="/sessions/create">Создать сессию</Link>
-                <Link to="/requests/create">Создать запрос</Link>
+                <DemoWidget></DemoWidget>
+                <AdminWidget></AdminWidget>
+                {/*<div className="Heading1" style={{color: "#7C7C7C"}}>*/}
+                {/*    Панель управления*/}
+                {/*</div>*/}
+                {/*<Link to="/sessions/create">Создать сессию</Link>*/}
+                {/*<Link to="/requests/create">Создать запрос</Link>*/}
             </div>);
     }
 }
